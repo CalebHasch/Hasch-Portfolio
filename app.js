3e732121a47e4ab1bf7c4d46c84dce787e2cd35a
Haschc <55303961+Haschc@users.noreply.github.com>
2020-05-06 21:16:57 -0600

    app changes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var { projects } = require('../data/data.json');
+var { projects } = require('./data/data.json');
 
 var app = express();
 
@@ -9,7 +9,7 @@ app.set('view engine', 'pug');
 app.use('/static', express.static('public'));
 
 app.get('/', (req, res) => {
-    res.locals = data.projects
+    res.locals = projects.data
     res.render('index', { projects });
 });
 
