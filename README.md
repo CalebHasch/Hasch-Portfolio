c69002d3efd4f7fbb0213b650c2209bf125bf3b7
Haschc <55303961+Haschc@users.noreply.github.com>
2020-05-07 20:42:29 -0600

    finishing touches/extra credit.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Tech-degree-project-6
- project 6
+ This project displays my other projects dynamically in a portfolio fashion.
+
+ I changed the font, background color, and added a text-shadow to all my buttons via css.
--- app.js
+++ app.js
@@ -22,24 +22,25 @@ app.get('/projects/:id', (req, res) => {
     const project = projects.find( ({ id }) => id === +projectId );
     
     if (project) {
-    res.render('project', { projects });
+        res.render('project', { project });
     } else {
         res.sendStatus(404);
     }
 });
+  
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+  });
 
-app.use((req, res, next) => {
-    // Log statement to indicate that this function is running 
-    console.log('Handling 404 error');
-  
-    // Create new error to handle non-existent routes
-    const err = new Error('err');
-    err.status = 404;
-    err.message = 'Woopsy, the page your looking for could not be found.';
-  
-    // Pass error to global error handler below
-    next(err);
-  })
+// error handler
+app.use(function(err, req, res, next) {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    res.status(err.status || 500);
+    res.render('error');
+});
 
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
