8ae8e0c0a28598e6490a2e251d31af456f14b558
Haschc <55303961+Haschc@users.noreply.github.com>
2020-05-09 22:11:43 -0600

    links, error and profile pic

--- app.js
+++ app.js
@@ -27,19 +27,19 @@ app.get('/projects/:id', (req, res) => {
         res.sendStatus(404);
     }
 });
-  
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    next(createError(404));
-  });
 
 // error handler
-app.use(function(err, req, res, next) {
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    res.status(err.status || 500);
-    res.render('error');
+app.use((req, res, next) => {
+        // Log statement to indicate that this function is running 
+        console.log('Handling 404 error');
+      
+        // Create new error to handle non-existent routes
+        const err = new Error('err');
+        err.status = 404;
+        err.message = 'Oops, page not found. Looks like that route does not exist.';
+      
+        // Pass error to global error handler below
+        next(err);
 });
 
 app.listen(3000, () => {
